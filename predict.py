8a428ad10056e9d0ae1411160af698d5ad90c8d6
Syed Ali Faraz <alii.exe3@gmail.com>
2026-04-24 22:39:23 +0500

    feat: enhance image classification feature with detailed results and visualizations

--- predict.py
+++ predict.py
@@ -5,19 +5,20 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
- 
 
 model = ResNet50(weights='imagenet')
 
 def predict_image(image_bytes):
     img = Image.open(BytesIO(image_bytes))
+    img = img.convert('RGB')
     img = img.resize((224, 224))
-    
+
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 
     predictions = model.predict(img_array)
+
     decoded = decode_predictions(predictions, top=5)[0]
 
     return decoded
